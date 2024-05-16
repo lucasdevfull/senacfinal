2b76f52d6de7b405ac4b2677475fae2e1812b232
Dante-Lucas <157182980+Dante-Lucas@users.noreply.github.com>
2024-05-15 21:23:29 -0300

    Add files via upload

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render,redirect
 from .models import User
+from .backend import MyBackend
+from django.urls import reverse
 from django.http import HttpResponse,HttpRequest
+from django.contrib import auth
+
 # Create your views here.
 def register(request:HttpRequest) -> HttpResponse:
     if request.method == 'GET':
@@ -12,12 +16,31 @@ def register(request:HttpRequest) -> HttpResponse:
         password = request.POST.get('password')
         confirm_password = request.POST.get('repeat_password')
 
-    try:
-        user=User.objects.create_user(username=username,email=email,password=password)
-        return HttpResponse(user)
-    except:
-        return HttpResponse('não foi') 
+        users = User.objects.filter(username=username,email=email)
+
+        if users.exists():
+            return HttpResponse('usuário já existe')
+        try:
+            user=User.objects.create_user(username=username,email=email,password=confirm_password)
+            return redirect(reverse(login))
+        except:
+            return HttpResponse('não foi') 
 
 def login(request:HttpRequest) -> HttpResponse:
+    if request.method == 'GET':
+    
+        template_name = "registration/login.html"
+        return render(request,template_name)
+    elif request.method == 'POST':
+    
+        username = request.POST.get('name')
+        password = request.POST.get('password')
+
+        users =auth.authenticate(request,username=username,password=password)
+        if users:
+            auth.login(request,users)
+            return redirect('/home/dashboard')
     template_name = "registration/login.html"
     return render(request,template_name)
+
+
