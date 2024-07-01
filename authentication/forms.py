7a7edaf1c7e58447f9ff933fd0295b084cd4872d
Dante-Lucas <157182980+Dante-Lucas@users.noreply.github.com>
2024-07-01 17:10:39 -0300

    Add files via upload

--- authentication/forms.py
+++ authentication/forms.py
@@ -2,7 +2,7 @@ from typing import Mapping
 from django.contrib.auth import forms as auth_forms
 from django import forms
 from .models import User 
-
+from django.utils.translation import gettext_lazy as _
 class UserChangeForm(auth_forms.UserChangeForm):
     class Meta(auth_forms.UserChangeForm.Meta):
         model = User
@@ -12,3 +12,11 @@ class UserCreationForm(auth_forms.UserCreationForm):
         model = User
 
 
+class PasswordResetForm(auth_forms.PasswordResetForm):
+    email = forms.EmailField(
+        label=_("Email"),
+        max_length=254,
+        widget=forms.EmailInput(attrs={"autocomplete": "email", 
+                                       "class": "block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                       }),
+    )
--- authentication/views.py
+++ authentication/views.py
@@ -1,20 +1,17 @@
 from django.db.utils import IntegrityError
 from django.shortcuts import render, redirect
-from .models import User
 from django.views import View
 from django.urls import reverse,reverse_lazy
 from django.http import HttpResponse,HttpRequest
 from django.contrib import auth
 from django.contrib import messages
 from django.contrib.messages import constants
-from backend.mixin import GetMixin
-
-class RegisterView(GetMixin,View):
+from django.contrib.auth.views import RedirectURLMixin
+from django.contrib.auth import get_user_model
+from backend.shortcuts import redirect_url
+User= get_user_model()
+class RegisterView(RedirectURLMixin,View):
     template_name: str = "registration/cadastro.html"
-    success_url = reverse_lazy('login')
-    register_url = reverse_lazy('register')
-    def get_success_url(self, url):
-        return super().get_success_url(url)
 
     def get(self, request:HttpRequest) -> HttpResponse:
         return render(request, self.template_name)
@@ -28,32 +25,26 @@ class RegisterView(GetMixin,View):
             
         users = User.objects.filter(username=username,email=email)
 
-        success_url = self.get_success_url(self.success_url)
-
-        redirect_url = super().get_redirect_url(self.register_url)
-
         if users.exists():
             
             messages.add_message(request,constants.ERROR,'Usuário já existente')
-            return redirect(redirect_url)
+            return redirect_url('register')
         try:
             users=User.objects.create_user(username=username,email=email,password=confirm_password, telefone=telefone)
             messages.add_message(request,constants.SUCCESS,'Usuário criado com sucesso')
             
-            return redirect(success_url)
+            return redirect_url('login')
             
         except IntegrityError:
             messages.add_message(request, constants.ERROR,f'Erro interno no servidor {IntegrityError}')
-            return redirect(redirect_url) 
+            return redirect_url('register') 
 
-class LoginView(GetMixin,View):      
+class LoginView(View):      
 
     template_name: str = "registration/login.html" 
-    success_url = reverse_lazy('listar')
-    login_url = reverse_lazy('login')
-    def get_success_url(self, url):
-        return super().get_success_url(url)
     def get(self, request: HttpRequest) -> HttpResponse:
+        if request.user.is_authenticated:
+            return redirect(reverse_lazy('listar'))
         return render(request, self.template_name)
     def post(self, request: HttpRequest) -> HttpResponse:
 
@@ -62,24 +53,16 @@ class LoginView(GetMixin,View):
         
         users = auth.authenticate(request,username=username,password=password)
         
-        success_url=self.get_success_url(self.success_url)
-        
-        login_url=self.get_redirect_url(self.login_url)
-        
         if users:
             auth.login(request, users)
-            return redirect(success_url)
+            return redirect_url('listar')
         messages.add_message(request,constants.ERROR,'Username ou senha inválidos')
-        return redirect(login_url)
-class LogoutView(GetMixin,View):
-    redirect_url = reverse_lazy('login')
-
-    def get_redirect_url(self, url):
-        return super().get_redirect_url(url)
+        return redirect_url('login')
+class LogoutView(View):
     
     def get(self, request: HttpRequest) -> HttpResponse:
         auth.logout(request)
-        url=self.get_redirect_url(self.redirect_url)
-        return redirect(url)
+        return redirect_url('login')
+
 
 
